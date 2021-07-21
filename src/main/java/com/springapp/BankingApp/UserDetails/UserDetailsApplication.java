186cfdae219523afe121eac348a4983ce4db2f02
Tuhin Pal <tuhinpal.434@gmail.com>
2021-07-21 18:30:48 +0530

    changing pom.xml

--- src/main/java/com/springapp/BankingApp/UserDetails/UserDetailsApplication.java
+++ src/main/java/com/springapp/BankingApp/UserDetails/UserDetailsApplication.java
@@ -2,21 +2,29 @@ package com.springapp.BankingApp.UserDetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class UserDetailsApplication {
+public class UserDetailsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(UserDetailsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserDetailsApplication.class, args);
 	}
 
+
+
 	@Bean
-	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
